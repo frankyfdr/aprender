3d9ba7972cabdc052519ffd7efccd09e00cef944
Frank-Lenovo <franklin-fdr@hotmail.com>
2020-09-21 22:01:01 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [text, setText] = useState();
+  useEffect(() => {
+    get();
+  });
+
+  const get = async () => {
+    await axios
+      .get("http://query1.finance.yahoo.com/v7/finance/quote?symbols=axp")
+      .then((data) => {
+        data = data.data.quoteResponse.result[0];
+        setText(data.shortName);
+      });
+  };
+
+  return (
+    <div>
+      <h1>API CORS Test</h1>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default App;
